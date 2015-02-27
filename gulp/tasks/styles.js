d8684906e8803fe24cb92bac9cd5c123133da86a
vasyan <vasilyator@gmail.com>
2015-02-27 11:30:40 +0100

    Spinner and waypoints dependencies

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var autoprefixer = require('gulp-autoprefixer');
 var config = require('../config.js').sass;
 
 gulp.task('styles', function() {
   gulp.src(config.src)
     .pipe(sass(config.settings))
+    .pipe(autoprefixer())
     .pipe(gulp.dest(config.dest))
     .pipe(connect.reload());
 });
