fffe8c79473045c09ce2f26e41763cde02e0962e
vasyan <vasilyator@gmail.com>
2015-03-13 21:43:07 +0100

    vk api service update

--- app/services/vk.js
+++ app/services/vk.js
@@ -1,11 +1,17 @@
-var request = require('request')
-	, _ = require('underscore');
+var request = require('request');
+var nodeUrl = require('url');
+var	_ = require('underscore');
+var Q = require('q');
+var Firebase = require('firebase');
+var publicsRef = new Firebase('https://foxyhunt.firebaseio.com/publics');
 
-var VK_API_URL = 'https://api.vk.com/method/',
-	options = {},
+var urlOptions = {
+		protocol: 'https',
+		host: 'api.vk.com',
+	},
 	apiMethodsOptions = {
 		'wall.get': {
-			'count': 100,
+			'count': 3,
 			'offset': 0,
 			'domain': 'lovekld39'
 		},
@@ -14,27 +20,75 @@ var VK_API_URL = 'https://api.vk.com/method/',
 				'bdate',
 				'sex',
 				'photo_big'
-				]
+				].join(',')
 		}
-	};
+	},
+	postCache,
+	usersCache,
+	usersTable = {};
 
-var getMethodUrl = function( method ) {
-	var apiMethodOptions = apiMethodsOptions[ method ];
-	var dataProps = _.keys(apiMethodOptions);
-	var url = _.map(dataProps, function( prop ) {
-		var values = apiMethodOptions[ prop ];
-		return prop + '=' + values.join('&');
-	}).join('');
+var extractUsersIds = function( posts ) {
+	console.log('Posts --------------------------------------- ', posts);
+	var id,
+		cache = usersCache;
+	_.forEach(posts, function( post ) {
+		id = post.signer_id;
+		if ( id && cache.indexOf(id) === -1) {
+			cache.push( id );
+		}
+	});
 
-	return VK_API_URL + method + url;
+	return usersCache;
+};
+
+var getMethodQuery = function( method, params ) {
+	var generatedOptions = _.extend( urlOptions,
+		{	query: apiMethodsOptions[ method ], pathname: 'method/' + method },
+		params
+	);
+
+	return nodeUrl.format( generatedOptions );
+};
+
+var getWallPosts = function( publicName, params ) {
+
+	console.log('getWallPosts run');
+	var wallUrl = getMethodQuery('wall.get', params);
+	return Q.Promise(function( resolve, reject ) {
+		request.get( wallUrl , function(err, res, body) {
+			if ( !err && res.statusCode === 200) {
+				postCache = JSON.parse( body ).response.slice(1);
+				extractUsersIds( postCache );
+				resolve( postCache );
+			} else {
+				reject( err );
+			}
+		});
+	});
+};
+
+var getUsersInfo = function( users ) {
+
+	console.log('getUsersInfo run');
+
+	var usersUrl = getMethodQuery('users.get', { uids: usersCache });
+	return Q.Promise(function( resolve, reject ) {
+		request.get( usersUrl, function( err, res ) {
+			if ( !err && res.statusCode === 200) {
+				usersCache = res;
+				resolve( res );
+			}
+		});
+	});
+};
+
+var getDataFromPublic = function( publicName, params ) {
+	getWallPosts( publicName, params ).then( getUsersInfo )
+		.then(function( payload ) {console.log('PAYLOAD', payload );});
+
 };
 
 module.exports = {
-  getDataFromPublic: function( url ) {
-
-  	// request( VK_API_URL + 'wall.get?count=100&offset=' + options.offset + '&domain=' + options.url)
-  },
-
-  _getMethodUrl: getMethodUrl,
-
+  getDataFromPublic: getDataFromPublic,
+  getWallPosts: getWallPosts
 };
